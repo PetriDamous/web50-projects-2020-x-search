b54f0163761401acae5678abcf707f07231dfdc3
dwilliams64 <dwilliams_64@yahoo.com>
2021-01-17 15:31:41 -0600

    Completed search page layout

---
+++ JS/index.js
@@ -0,0 +1,22 @@
+const searchInput = document.getElementById("search__input");
+const searchBox = document.getElementById("search__box");
+const searchClear = document.getElementById("search__clear");
+
+const boxShadowSearch = "0 1px 6px rgba(32,33,36,.28)";
+
+searchBox.addEventListener("focusin", e => {
+    searchInput.style.boxShadow = boxShadowSearch;
+});
+
+searchBox.addEventListener("focusout", e => {
+    searchInput.style.boxShadow = "none";
+});
+
+searchBox.addEventListener("input", e => {
+    searchClear.style.display = e.target.value !== "" ? "block" : "none";
+});
+
+searchClear.addEventListener("click", e => {
+    searchBox.value = "";
+    searchClear.style.display = "none";
+});
