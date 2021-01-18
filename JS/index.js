0c8a9fb3500774707629d8a260f60b3f9fb3f9b2
dwilliams64 <dwilliams_64@yahoo.com>
2021-01-18 12:11:53 -0600

    completed basic image search layout

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,8 @@ const searchInput = document.getElementById("search__input");
 const searchBox = document.getElementById("search__box");
 const searchClear = document.getElementById("search__clear");
 
+const searchLogo = document.getElementById("search__logo");
+
 const boxShadowSearch = "0 1px 6px rgba(32,33,36,.28)";
 
 searchBox.addEventListener("focusin", e => {
